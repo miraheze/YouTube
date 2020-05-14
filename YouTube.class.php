cb7dc91703adb98e0a82a1fdb2fe0e785fedf629
Elaeagnifolia <j.wong205914@gmail.com>
2020-05-14 13:40:04 -0500

    Change to only enable NicoVideo

--- YouTube.class.php
+++ YouTube.class.php
@@ -39,11 +39,11 @@ class YouTube {
 		$parser->setHook( 'gvideo', array( __CLASS__, 'embedGoogleVideo' ) );
 		$parser->setHook( 'aovideo', array( __CLASS__, 'embedArchiveOrgVideo' ) );
 		$parser->setHook( 'aoaudio', array( __CLASS__, 'embedArchiveOrgAudio' ) );
-		$parser->setHook( 'wegame', array( __CLASS__, 'embedWeGame' ) );
+		$parser->setHook( 'nicovideo', array( __CLASS__, 'embedNicovideo' ) );
+		/* $parser->setHook( 'wegame', array( __CLASS__, 'embedWeGame' ) );
 		$parser->setHook( 'tangler', array( __CLASS__, 'embedTangler' ) );
 		$parser->setHook( 'gtrailer', array( __CLASS__, 'embedGametrailers' ) );
-		$parser->setHook( 'nicovideo', array( __CLASS__, 'embedNicovideo' ) );
-		$parser->setHook( 'ggtube', array( __CLASS__, 'embedGoGreenTube' ) );
+		$parser->setHook( 'ggtube', array( __CLASS__, 'embedGoGreenTube' ) ); */
 		return true;
 	}
 
@@ -293,7 +293,7 @@ class YouTube {
 		}
 	}
 
-	public static function url2weid( $url ) {
+	/* public static function url2weid( $url ) {
 		$id = $url;
 
 		if ( preg_match( '/^http:\/\/www\.wegame\.com\/watch\/(.+)\/$/', $url, $preg ) ) {
@@ -417,7 +417,7 @@ class YouTube {
 			// gametrailers' flash doesn't work on FF with object tag alone )-: weird, yt and gvideo are ok )-: valid xhtml no more )-:
 			return "<object classid=\"clsid:d27cdb6e-ae6d-11cf-96b8-444553540000\"  codebase=\"http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=8,0,0,0\" id=\"gtembed\" width=\"{$width}\" height=\"{$height}\">	<param name=\"allowScriptAccess\" value=\"sameDomain\" /> 	<param name=\"allowFullScreen\" value=\"true\" /> <param name=\"movie\" value=\"{$url}\"/> <param name=\"quality\" value=\"high\" /> <embed src=\"{$url}\" swLiveConnect=\"true\" name=\"gtembed\" align=\"middle\" allowScriptAccess=\"sameDomain\" allowFullScreen=\"true\" quality=\"high\" pluginspage=\"http://www.macromedia.com/go/getflashplayer\" type=\"application/x-shockwave-flash\" width=\"{$width}\" height=\"{$height}\"></embed> </object>";
 		}
-	}
+	} */
 
 	public static function url2nvid( $url ) {
 		$id = $url;
@@ -459,7 +459,7 @@ class YouTube {
 		}
 	}
 
-	public static function url2ggid( $url ) {
+	/* public static function url2ggid( $url ) {
 		$id = $url;
 
 		if ( preg_match( '/^http:\/\/www\.gogreentube\.com\/watch\.php\?v=(.+)$/', $url, $preg ) ) {
@@ -507,6 +507,6 @@ class YouTube {
 			$url = "http://www.gogreentube.com/embed/{$ggid}";
 			return "<script type=\"text/javascript\" src=\"{$url}\"></script>";
 		}
-	}
+	} */
 
 }
